c40150778440a72e96e430339efed3ef10cfa318
Sidak <dhillonsidak@gmail.com>
2021-03-15 03:36:47 -0400

    added router file to handle incoming requests

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 // Main starting point of the application
 const express = require("express");
 const http = require("http");
-const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const router = require("./router");
 
 // App Setup
 const app = express();
+app.use(morgan("combined"));
+app.use(express.json());
+router(app);
 
 // Server Setup
 const port = process.env.PORT || 3000;
