da5de796f82287da1701f2f2aa63124c7ec93903
Sidak <dhillonsidak@gmail.com>
2021-03-15 05:23:31 -0400

    signup route

--- server/router.js
+++ server/router.js
@@ -1,5 +1,9 @@
+const auth = require("./controllers/auth.controller");
+
 module.exports = (app) => {
   app.get("/", (req, res, next) => {
     res.send(["hello", "world"]);
   });
+
+  app.post("/signup", auth.signup);
 };
