208644e0be199893d6a7dd4476dbcbe03a5d993b
Sidak <dhillonsidak@gmail.com>
2021-03-15 06:40:13 -0400

    middleware to authenticate users

--- server/router.js
+++ server/router.js
@@ -1,7 +1,11 @@
 const auth = require("./controllers/auth.controller");
+const passportService = require("./services/passport");
+const passport = require("passport");
+
+const requireAuth = passport.authenticate("jwt", { session: false });
 
 module.exports = (app) => {
-  app.get("/", (req, res, next) => {
+  app.get("/", requireAuth, (req, res, next) => {
     res.send(["hello", "world"]);
   });
 
